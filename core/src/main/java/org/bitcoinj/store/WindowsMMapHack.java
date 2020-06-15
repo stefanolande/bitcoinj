faf3e2994a654b82088145b6a9e77584d92aa11d
Stefano Lande <landeste@gmail.com>
2020-06-15 13:04:35 +0000

    removed windows specific hack

--- core/src/main/java/org/bitcoinj/store/WindowsMMapHack.java
+++ core/src/main/java/org/bitcoinj/store/WindowsMMapHack.java
@@ -33,7 +33,7 @@ import java.nio.*;
  */
 public class WindowsMMapHack {
     public static void forceRelease(MappedByteBuffer buffer) {
-        Cleaner cleaner = ((DirectBuffer) buffer).cleaner();
-        if (cleaner != null) cleaner.clean();
+        //Cleaner cleaner = ((DirectBuffer) buffer).cleaner();
+        //if (cleaner != null) cleaner.clean();
     }
 }
